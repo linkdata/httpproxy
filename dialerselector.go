ef5dce8427297f3b995cc07ea074da4b558cd45f
Johan Lindh <johan@linkdata.se>
2025-07-11 11:12:55 +0200

    wip

--- dialerselector.go
+++ dialerselector.go
@@ -5,5 +5,7 @@ type DialerSelector interface {
 	// SelectDialer returns the ContextDialer to use.
 	//
 	// If username is the empty string no authorization has taken place (anonymous usage).
+	// If you return the error httpproxy.ErrUnauthorized then httpproxy will generate
+	// the HTTP status code 401 Unauthorized.
 	SelectDialer(username, network, address string) (cd ContextDialer, err error)
 }
