f85b98216acfd53ecb285e3dd8c0ef4786aa2ff8
Johan Lindh <johan@linkdata.se>
2025-07-11 12:50:27 +0200

    wip

--- basicauth.go
+++ basicauth.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func BasicAuth(hdr http.Header) (username, password string, err error) {
+func ReadBasicAuth(hdr http.Header) (username, password string, err error) {
 	if authkind, encoding, found := strings.Cut(hdr.Get("Proxy-Authorization"), " "); found && authkind == "Basic" {
 		var userpassraw []byte
 		if userpassraw, err = base64.StdEncoding.DecodeString(encoding); err == nil {
