d15b0b74849744499d443cd74703b0615ddcfe4d
Johan Lindh <johan@linkdata.se>
2025-07-11 11:53:43 +0200

    wip

--- proxy_test.go
+++ proxy_test.go
@@ -9,35 +9,47 @@ import (
 	"testing"
 )
 
+var testBody = []byte("Hello world!")
+
 func maybeFatal(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
-func makeClient(s *httptest.Server) *http.Client {
-	u, _ := url.Parse(s.URL)
+func makeDestSrv(t *testing.T) *httptest.Server {
+	t.Helper()
+	destsrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(testBody)
+	}))
+	return destsrv
+}
+
+func makeClient(t *testing.T, urlstr string) *http.Client {
+	t.Helper()
+	u, err := url.Parse(urlstr)
+	if err != nil {
+		t.Fatal(err)
+	}
 	tr := &http.Transport{Proxy: http.ProxyURL(u)}
 	return &http.Client{Transport: tr}
 }
 
 func TestSimpleHTTPRequest(t *testing.T) {
-	bodyText := []byte("Hello world!")
-
-	destsrv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(bodyText)
-	}))
+	destsrv := makeDestSrv(t)
 	defer destsrv.Close()
 
 	proxysrv := httptest.NewServer(&Server{})
 	defer proxysrv.Close()
 
-	resp, err := makeClient(proxysrv).Get(destsrv.URL)
+	resp, err := makeClient(t, proxysrv.URL).Get(destsrv.URL)
 	maybeFatal(t, err)
 
 	body, err := io.ReadAll(resp.Body)
 	maybeFatal(t, err)
-	if !bytes.Equal(body, bodyText) {
-		t.Errorf("status %q: got %q, wanted %q\n", resp.Status, string(body), string(bodyText))
+
+	if !bytes.Equal(body, testBody) {
+		t.Errorf("status %q: got %q, wanted %q\n", resp.Status, string(body), string(testBody))
 	}
 }
