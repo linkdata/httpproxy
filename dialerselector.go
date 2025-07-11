edfd0b82ed26240241f2f4de24d47b7b613128d2
Johan Lindh <johan@linkdata.se>
2025-07-11 11:11:06 +0200

    wip

--- dialerselector.go
+++ dialerselector.go
@@ -3,5 +3,7 @@ package httpproxy
 // A DialerSelector returns the ContextDialer to use.
 type DialerSelector interface {
 	// SelectDialer returns the ContextDialer to use.
+	//
+	// If username is the empty string no authorization has taken place (anonymous usage).
 	SelectDialer(username, network, address string) (cd ContextDialer, err error)
 }
